a9e34a1ccec01e256647d3a29a0b50b84028dabd
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:57:22 +0900

    Add tutorial 6

---
+++ Tutorial_6/Tutorial_6/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[10]
+{
+    1, 6, 9, 4, 6, 9, 7, 4, 1, 5
+};
+
+array[2] = 10;
+array[6] *= 2;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
