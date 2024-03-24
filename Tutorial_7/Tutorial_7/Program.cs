90a92ca77df5e450e1087c03021a32036c99abae
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:57:32 +0900

    Add tutorial 7

---
+++ Tutorial_7/Tutorial_7/Program.cs
@@ -0,0 +1,29 @@
+﻿void Print()
+{
+    Console.WriteLine("Print");
+}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+void Write(string message, bool nextLine = false)
+{
+    if (nextLine)
+    {
+        Console.WriteLine(message);
+    }
+    else
+    {
+        Console.Write(message);
+    }
+}
+
+Print();
+
+Console.WriteLine(Add(10, 5));
+
+Write("Hello ");
+Write("World!", true);
+Write("Hi!", false);
