faa6bc7cd45b6ecc290e28776bb8f56ace7f369a
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:58:06 +0900

    Add tutorial 10

---
+++ Tutorial_10/Tutorial_10/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace A
+{
+    class Test
+    {
+        public static void Print()
+        {
+            Console.WriteLine("A.Test.Print()");
+        }
+    }
+}
+
+namespace B
+{
+    class Test
+    {
+        public static void Print()
+        {
+            Console.WriteLine("B.Test.Print()");
+        }
+    }
+}
+
+class Test
+{
+    static void Main()
+    {
+        A.Test.Print();
+        B.Test.Print();
+    }
+}
