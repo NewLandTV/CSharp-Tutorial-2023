ea6ebcc6f9395ae8b73ca238786b76bf48d17d83
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:57:11 +0900

    Add tutorial 5

---
+++ Tutorial_5/Tutorial_5/Program.cs
@@ -0,0 +1,20 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+int j = 0;
+
+while (true)
+{
+    Console.WriteLine("While");
+
+    j++;
+
+    if (j >= 10)
+    {
+        break;
+    }
+}
+
+Console.WriteLine("End");
