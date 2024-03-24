3dc799aea0f05ae024d1c65bf457ba0a0c853cc2
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:56:42 +0900

    Add tutorial 2

---
+++ Tutorial_2/Tutorial_2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 5;  // 정수 4Byte
+float f = 3.14f;    // 실수 4Byte
+double d = 0.00012342332d;  // 실수 8Byte
+string str = "\\ \"Hello";   // 문자열 (\ "Hello)
+
+Console.WriteLine(a);
+Console.WriteLine(f);
+Console.WriteLine(d);
+Console.WriteLine(str);
