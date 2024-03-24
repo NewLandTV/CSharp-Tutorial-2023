5cce6752720f22fa96e633bba510317f25198c29
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:56:52 +0900

    Add tutorial 3

---
+++ Tutorial_3/Tutorial_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5;
+int b = 3;
+string str1 = "Hello ";
+string str2 = "World!";
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+
+a = a + 5;  // a += 5
+
+Console.WriteLine(a);
+Console.WriteLine(str1 + str2 + " + Hi");
