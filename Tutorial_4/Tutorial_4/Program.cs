4e8ca7c240a39b73c7a5c05fea1e311b698c774d
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:57:02 +0900

    Add tutorial 4

---
+++ Tutorial_4/Tutorial_4/Program.cs
@@ -0,0 +1,26 @@
+﻿bool condition = false;
+
+if (condition)
+{
+    Console.WriteLine("True");
+}
+else if (!condition)
+{
+    Console.WriteLine("False");
+
+    int a = 5;
+    int b = 0;
+
+    if (a + b == 8)
+    {
+        Console.WriteLine("a + b = 8");
+    }
+    else if (a + b == 0)    // (a < b || b >= a) && true
+    {
+        Console.WriteLine("a + b = 0");
+    }
+    else
+    {
+        Console.WriteLine(a + b);
+    }
+}
