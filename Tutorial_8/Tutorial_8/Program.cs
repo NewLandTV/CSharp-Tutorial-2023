783e70c4d6eb8f7e244f348c08646af3dd90f12b
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-24 19:57:44 +0900

    Add tutorial 8

---
+++ Tutorial_8/Tutorial_8/Program.cs
@@ -0,0 +1,35 @@
+﻿class Person
+{
+    private int age;
+    private string name;
+
+    public Person(int age, string name)
+    {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
+
+class Test
+{
+    static void Main()
+    {
+        Person person = new Person(13, "Test");
+        Person person2 = new Person(21, "가다나");
+
+        Console.WriteLine(person.GetAge());
+        Console.WriteLine(person.GetName());
+        Console.WriteLine(person2.GetAge());
+        Console.WriteLine(person2.GetName());
+    }
+}
